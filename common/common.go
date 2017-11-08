3e12b5e516422c767ba0778173dbe35a33abe73b
tyler <tyler@blend.com>
2017-11-08 14:08:53 -0800

    gofmt

--- common/common.go
+++ common/common.go
@@ -1,11 +1,10 @@
 package common
 
-// 
+//
 type InstanceIdentityDocument struct {
 	InstanceId string `json:"instanceId" `
 	AccountId  string `json:"accountId"`
 }
-
 
 type IidAttestedData struct {
 	Document  string `json:"document"`
